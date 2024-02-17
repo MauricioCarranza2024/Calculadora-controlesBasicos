c941f93372093efa211b97ec9745e9265506f24a
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-02-16 21:10:02 -0600

    Creacion de la rama sensor de acelerometro y el codigo creado en la clase

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -9,10 +9,8 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TabHost;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     SensorManager sensorManager;
