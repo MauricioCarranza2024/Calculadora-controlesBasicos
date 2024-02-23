c191a8ef502f2077dd10bd0989ab6631026dbfde
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-02-23 13:09:09 -0600

    Creacion de la rama y el codigo sensor de GPS.

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -1,35 +1,27 @@
 package com.ugb.controlesbasicos;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TabHost;
 import android.widget.TextView;
-
-
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
-
-        TabHost tbh;
+    TabHost tbh;
+    TextView tempval;
+    Spinner spn;
+    Button btnLongitud;
 
     conversores miObj = new conversores();
-
     @Override
-    protected void onCreate(Bundle saveInstanceState) {
-        super.onCreate(saveInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         tbh = findViewById(R.id.tbhConversor);
         tbh.setup();
         tbh.addTab(tbh.newTabSpec("LON").setContent(R.id.tabLongitud).setIndicator("LONGITUD", null));
+
 
         btnLongitud = findViewById(R.id.btnConvertirLongitud);
         btnLongitud.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +41,3 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-    }
-}
