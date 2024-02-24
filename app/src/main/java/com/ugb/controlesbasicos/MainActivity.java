a86069b4baae699c445fdff5269b8b532f5ad548
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-02-23 19:47:26 -0600

    Creacion de la rama parcial_1, y realizacion del codigo perteneciente al Parcial I de programacion II

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnArea = findViewById(R.id.btnConvertirArea);
         btnArea.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 try {
