af5c9108e6700af325a4acdce227f77d7b847ff3
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-03-01 14:20:56 -0600

    Creacion de la rama agendaAmigos_SQLite y clase DB donde se heredo de la clase SQLiteOpenHelper al igual que los metodos para la administracion de la BD y los datos

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -1,7 +1,8 @@
 package com.ugb.controlesbasicos;
 
-
+import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,57 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends Activity{
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+
+public class MainActivity extends AppCompatActivity {
+    TextView tempVal;
+    Button btn;
+    FloatingActionButton btnRegresar;
+    String id="", accion="nuevo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnRegresar = findViewById(R.id.fabListaAmigos);
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regresarLista = new Intent(getApplicationContext(), lista_amigos.class);
+                startActivity(regresarLista);
+            }
+        });
+
+        btn = findViewById(R.id.btnGuardarAmigos);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              tempVal = findViewById(R.id.txtnombre);
+              String nombre = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtdireccion);
+                String direccion = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtTelefono);
+                String telefono = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtEmail);
+                String email = tempVal.getText().toString();
+
+                tempVal = findViewById(R.id.txtDui);
+                String dui = tempVal.getText().toString();
+
+                String[] datos = new String[]{id,nombre,direccion,telefono,email,dui};
+                DB db = new DB(getApplicationContext(),"",null,1);
+                String respuesta = db.administrar_amigos(accion, datos);
+                if (respuesta.equals("ok") ){
+                    Toast.makeText(getApplicationContext(), "Amigo registrado con exito", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),"Error al intentar registrar amigo "+ respuesta, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
