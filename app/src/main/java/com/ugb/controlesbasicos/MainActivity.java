f9034884f413c3e5a29a6fc102ed9091de513f6e
MauricioCarranza2024 <wichovelasquez413@gmail.com>
2024-03-12 16:18:47 -0600

    Modificacion y mejora de todo el codigo de agendaAmigos_SQLite

--- app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
+++ app/src/main/java/com/ugb/controlesbasicos/MainActivity.java
@@ -1,25 +1,23 @@
 package com.ugb.controlesbasicos;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 
 public class MainActivity extends AppCompatActivity {
     TextView tempVal;
     Button btn;
     FloatingActionButton btnRegresar;
     String id="", accion="nuevo";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,24 +26,23 @@ public class MainActivity extends AppCompatActivity {
         btnRegresar = findViewById(R.id.fabListaAmigos);
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent regresarLista = new Intent(getApplicationContext(), lista_amigos.class);
                 startActivity(regresarLista);
             }
         });
-
-        btn = findViewById(R.id.btnGuardarAmigos);
+        btn = findViewById(R.id.btnGuardarAmigo);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-              tempVal = findViewById(R.id.txtnombre);
-              String nombre = tempVal.getText().toString();
+            public void onClick(View view) {
+                tempVal = findViewById(R.id.txtnombre);
+                String nombre = tempVal.getText().toString();
 
                 tempVal = findViewById(R.id.txtdireccion);
                 String direccion = tempVal.getText().toString();
 
                 tempVal = findViewById(R.id.txtTelefono);
-                String telefono = tempVal.getText().toString();
+                String tel = tempVal.getText().toString();
 
                 tempVal = findViewById(R.id.txtEmail);
                 String email = tempVal.getText().toString();
@@ -53,15 +50,23 @@ public class MainActivity extends AppCompatActivity {
                 tempVal = findViewById(R.id.txtDui);
                 String dui = tempVal.getText().toString();
 
-                String[] datos = new String[]{id,nombre,direccion,telefono,email,dui};
-                DB db = new DB(getApplicationContext(),"",null,1);
+                String[] datos = new String[]{id,nombre,direccion,tel,email,dui};
+                DB db = new DB(getApplicationContext(),"", null, 1);
                 String respuesta = db.administrar_amigos(accion, datos);
-                if (respuesta.equals("ok") ){
-                    Toast.makeText(getApplicationContext(), "Amigo registrado con exito", Toast.LENGTH_LONG).show();
+                if( respuesta.equals("ok") ){
+                    mostrarMsg("Amigos registrado con exito.");
+                    listarAmigos();
                 }else {
-                    Toast.makeText(getApplicationContext(),"Error al intentar registrar amigo "+ respuesta, Toast.LENGTH_LONG).show();
+                    mostrarMsg("Error al intentar registrar el amigo: "+ respuesta);
                 }
             }
         });
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+    }
+    private void listarAmigos(){
+        Intent intent = new Intent(getApplicationContext(), lista_amigos.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ugb/controlesbasicos/lista_amigos.java
+++ app/src/main/java/com/ugb/controlesbasicos/lista_amigos.java
@@ -3,14 +3,27 @@ package com.ugb.controlesbasicos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class lista_amigos extends AppCompatActivity {
     FloatingActionButton btnAgregarAmigos;
-
+    ListView lts;
+    Cursor cAmigos;
+    amigos misAamigos;
+    DB db;
+    final ArrayList<amigos> alAmigos=new ArrayList<amigos>();
+    final ArrayList<amigos> alAmigosCopy=new ArrayList<amigos>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +32,95 @@ public class lista_amigos extends AppCompatActivity {
         btnAgregarAmigos = findViewById(R.id.fabAgregarAmigos);
         btnAgregarAmigos.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 abrirActividad();
             }
         });
+        obtenerDatosAmigos();
+        buscarAmigos();
     }
     private void abrirActividad(){
         Intent abrirActividad = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(abrirActividad);
+    }
+    private void obtenerDatosAmigos(){
+        try {
+            alAmigos.clear();
+            alAmigosCopy.clear();
+
+            db = new DB(lista_amigos.this, "", null, 1);
+            cAmigos = db.consultar_amigos();
+
+            if( cAmigos.moveToFirst() ){
+                lts = findViewById(R.id.ltsAmigos);
+                do{
+                    misAamigos = new amigos(
+                            cAmigos.getString(0),//idAmigo
+                            cAmigos.getString(1),//nombre
+                            cAmigos.getString(2),//direccion
+                            cAmigos.getString(3),//telefono
+                            cAmigos.getString(4),//email
+                            cAmigos.getString(5)//dui
+                    );
+                    alAmigos.add(misAamigos);
+                }while(cAmigos.moveToNext());
+                alAmigosCopy.addAll(alAmigos);
+
+                adaptadorImagenes adImagenes = new adaptadorImagenes(lista_amigos.this, alAmigos);
+                lts.setAdapter(adImagenes);
+
+                registerForContextMenu(lts);
+            }else{
+                mostrarMsg("No hay Datos de amigos que mostrar.");
+            }
+        }catch (Exception e){
+            mostrarMsg("Error al mostrar datos: "+ e.getMessage());
+        }
+    }
+    private void buscarAmigos(){
+        TextView tempVal;
+        tempVal = findViewById(R.id.txtBuscarAmigos);
+        tempVal.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    alAmigos.clear();
+                    String valor = tempVal.getText().toString().trim().toLowerCase();
+                    if( valor.length()<=0 ){
+                        alAmigos.addAll(alAmigosCopy);
+                    }else{
+                        for (amigos amigo : alAmigosCopy){
+                            String nombre = amigo.getNombre();
+                            String direccion = amigo.getDireccion();
+                            String tel = amigo.getTelefono();
+                            String email = amigo.getEmail();
+                            String dui = amigo.getDui();
+                            if(nombre.toLowerCase().trim().contains(valor) ||
+                                    direccion.toLowerCase().trim().contains(valor) ||
+                                    tel.trim().contains(valor) ||
+                                    email.trim().toLowerCase().contains(valor) ||
+                                    dui.trim().contains(valor)){
+                                alAmigos.add(amigo);
+                            }
+                        }
+                        adaptadorImagenes adImagenes = new adaptadorImagenes(getApplicationContext(), alAmigos);
+                        lts.setAdapter(adImagenes);
+                    }
+                }catch (Exception e){
+                    mostrarMsg("Error al buscar: "+ e.getMessage());
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+    private void mostrarMsg(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
 }
